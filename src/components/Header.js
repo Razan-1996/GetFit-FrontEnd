b2a5de2370fe0c76fb0dc3e3f700176f0a0f0e8d
hatimalattas <alattas96@outlook.com>
2020-02-04 09:54:25 +0300

    trying to fix user

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ import Product from './Product';
 import Macro from '../MacroCalculator/components/Macro';
 import axios from 'axios';
 import Logout from './validation/Logout';
-import SingUp from './validation/SingUp';
-import SignIn from './validation/SignIn';
+import Signin from './validation/Signup';
+import Signup from './validation/Signin';
 
 import {
   BrowserRouter as Router,
@@ -15,94 +15,167 @@ import {
   Link
 } from 'react-router-dom'; 
 
-
-class Header extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      isLoggedIn: false
-    }
-  }
-
-  // componentDidMount=()=>{
-  //   axios.get("http://localhost:3000/users/sign_in").then(res => {
-  //       this.setState({isLoggedIn: res.data})
-  //   });
-  // }
-
-  // loginStatus = (logged_in) =>{
-  //   this.setState({isLoggedIn: logged_in})
-  // }
-
-  render () {
-
-    const loggedInLinks = (
-      <>
-      {/* <Link className="navbar-brand" to="/add_menu_item">Add Menu Item</Link>{" "} */}
-      <Link className="navbar-brand" to="/logout">Logout</Link>{" "}
-      </>
-  )
-
-  const loggedInRoutes = (
-      <>
-      {/* <Route path="/add_menu_item" component={AddMenuItem} /> */}
-      <Route path="/logout" component={() => <Logout loginStatus={this.loginStatus}/>} />
-      </>
-  )
-
-  const loggedOutLinks = (
-      <>
-      <Link className="navbar-brand" to="/signin">SignIn</Link>{" "}
-      <Link className="navbar-brand" to="/signup">SignUp</Link>{" "}
-      </>
-  )
-
-  const loggedOutRoutes = (
-      <>
-      <Route path="/signin" component={() => <SignIn loginStatus={this.loginStatus}/>} />
-      <Route path="/signup" component={SingUp} />
-      </>
-  )
-
+class Header extends React.Component {
+  // constructor(props){
+  //     super(props);
+  //     if (this.props.currentUser == null){
+  //       this.state = {
+  //         page:"login"
+  //       }
+  //     } else{
+  //       this.state = {
+  //         page: "delete"
+  //       }
+  //     }
+  //     this.changePage = this.changePage.bind(this);
+  //   }
+  // changePage(newPage) {
+  //     this.setState({
+  //       page: newPage
+  //     })
+  //   }
+  render() {
+      // switch(this.state.page) {
+      //   case "signup":
+      //     return <Signup changePage={this.changePage} updateCurrentUser={this.props.updateCurrentUser}/>
+      //   case "login":
+      //     return <Signin changePage={this.changePage} updateCurrentUser={this.props.updateCurrentUser}/>
+      //   case "delete":
+      //     return <Logout changePage={this.changePage} updateCurrentUser={this.props.updateCurrentUser}/>
+      // }
+    
     return (
 
-      <Router>
-<nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-    <div className="container">
-      <a className="navbar-brand js-scroll-trigger"><Link to="/">GetFit</Link></a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav ml-auto my-2 my-lg-0">
+            <Router>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+          <div className="container">
+            <a className="navbar-brand js-scroll-trigger"><Link to="/">GetFit</Link></a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto my-2 my-lg-0">
+      
+                <li className="nav-item">
+              <a className="nav-link js-scroll-trigger"><Link to="/Diet">Diet</Link></a>
+                </li>
+      
+                <li className="nav-item">
+              <a className="nav-link js-scroll-trigger"><Link to="/Product">Product</Link></a>
+                </li>
+      
+                <li className="nav-item">
+              <a className="nav-link js-scroll-trigger"><Link to="/Workout">Workout</Link></a>
+                </li>
+      
+                <li className="nav-item">
+              <a className="nav-link js-scroll-trigger"><Link to="/MacroCalculator">Macro Calculator</Link></a>
+                </li>
+      
+  
+      
+              </ul>
+            </div>
+          </div>
+        </nav>
+      
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Diet" component={Diet} />
+        <Route exact path="/MacroCalculator" component={Macro} />
+        <Route exact path="/Product" component={Product} />
 
-          <li className="nav-item">
-        <a className="nav-link js-scroll-trigger"><Link to="/Diet">Diet</Link></a>
-          </li>
+        </Router>
+         )}}
+  
 
-          <li className="nav-item">
-        <a className="nav-link js-scroll-trigger"><Link to="/Product">Product</Link></a>
-          </li>
+// class Header extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       isLoggedIn: false
+//     }
+//   }
 
-          <li className="nav-item">
-        <a className="nav-link js-scroll-trigger"><Link to="/Workout">Workout</Link></a>
-          </li>
+//   componentDidMount=()=>{
+//     axios.get("http://localhost:3000/users/sign_in", {headers:{ "Authorization": localStorage.getItem("token")}}).then(res => {
+//         this.setState({isLoggedIn: res.data})
+//     });
+//   }
 
-          <li className="nav-item">
-        <a className="nav-link js-scroll-trigger"><Link to="/MacroCalculator">Macro Calculator</Link></a>
-          </li>
-          {this.state.isLoggedIn? loggedInLinks : loggedOutLinks }
-        </ul>
-      </div>
-    </div>
-  </nav>
+//   loginStatus = (logged_in) =>{
+//     this.setState({isLoggedIn: logged_in})
+//   }
 
-  <Route exact path="/" component={Home} />
-  <Route exact path="/Diet" component={Diet} />
-  <Route exact path="/MacroCalculator" component={Macro} />
-  <Route exact path="/Product" component={Product} />
-  {this.state.isLoggedIn? loggedInRoutes: loggedOutRoutes}
-  </Router>
-   )}}
+//   render () {
+//     const loggedInLinks = (
+//       <>
+//       {/* <Link className="navbar-brand" to="/add_menu_item">Add Menu Item</Link>{" "} */}
+//       <Link className="navbar-brand" to="/logout">Logout</Link>{" "}
+//       </>
+//   )
+
+//   const loggedInRoutes = (
+//       <>
+//       {/* <Route path="/add_menu_item" component={AddMenuItem} /> */}
+//       <Route path="/logout" component={() => <Logout loginStatus={this.loginStatus}/>} />
+//       </>
+//   )
+
+//   const loggedOutLinks = (
+//       <>
+//       <Link className="navbar-brand" to="/signin">SignIn</Link>{" "}
+//       <Link className="navbar-brand" to="/signup">SignUp</Link>{" "}
+//       </>
+//   )
+
+//   const loggedOutRoutes = (
+//       <>
+//       <Route path="/signin" component={() => <SignIn loginStatus={this.loginStatus}/>} />
+//       <Route path="/signup" component={SingUp} />
+//       </>
+//   )
+  
+//     return (
+
+//       <Router>
+// <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+//     <div className="container">
+//       <a className="navbar-brand js-scroll-trigger"><Link to="/">GetFit</Link></a>
+//       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+//       <div className="collapse navbar-collapse" id="navbarResponsive">
+//         <ul className="navbar-nav ml-auto my-2 my-lg-0">
+
+//           <li className="nav-item">
+//         <a className="nav-link js-scroll-trigger"><Link to="/Diet">Diet</Link></a>
+//           </li>
+
+//           <li className="nav-item">
+//         <a className="nav-link js-scroll-trigger"><Link to="/Product">Product</Link></a>
+//           </li>
+
+//           <li className="nav-item">
+//         <a className="nav-link js-scroll-trigger"><Link to="/Workout">Workout</Link></a>
+//           </li>
+
+//           <li className="nav-item">
+//         <a className="nav-link js-scroll-trigger"><Link to="/MacroCalculator">Macro Calculator</Link></a>
+//           </li>
+
+//           {this.state.isLoggedIn? loggedInLinks : loggedOutLinks}
+
+//         </ul>
+//       </div>
+//     </div>
+//   </nav>
+
+//   <Route exact path="/" component={Home} />
+//   <Route exact path="/Diet" component={Diet} />
+//   <Route exact path="/MacroCalculator" component={Macro} />
+//   <Route exact path="/Product" component={Product} />
+//   {this.state.isLoggedIn ? loggedInRoutes: loggedOutRoutes}
+//   </Router>
+//    )}}
 
 export default Header;
--- src/components/validation/SignIn.js
+++ src/components/validation/SignIn.js
@@ -4,114 +4,195 @@ import axios from 'axios'
 // import Loading from '../addson/Loading'
 import { toast } from 'react-toastify';
 
-
-let base_url = 'http://localhost:3000/users/sign_in'
-
-
-export default class SignIn extends React.Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            email:"",
-            password:"",
-            isActive: false
+class Signin extends React.Component {
+    constructor(props){
+      super(props);
+      this.handleSignup = this.handleSignup.bind(this);
+    }
+  handleSignup(e) {
+      e.preventDefault();
+      let that = this
+      let email = this.props.currentUser
+      axios.post('http://localhost:3000/users/sign_in', {
+        user: {
+          email: document.getElementById("email").value,
+          password: document.getElementById("password").value,
+          password_confirmation: document.getElementById("password_confirmation").value
         }
-    }
-
-    handleChange = e => {
-        e.preventDefault()
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    handleSubmit = () => {
-        setTimeout(() => {
-            this.setState(prevState => {return {isActive: !prevState.isActive}})
-            axios.post(`${base_url}/users/sign_in` , 
-                {auth: {email: this.state.email, password: this.state.password}}
-            )
-            .then(r=>{
-                
-                console.log(r);
-                if (r.data.jwt) {
-                    localStorage.setItem('token', r.data.jwt)
-                    this.props.loginStatus(true)
-                    toast.success("Logged In successfully", {
-                        position: "top-center",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: false,
-                        pauseOnHover: false,
-                        draggable: false,
-                    });
-                    // alert.success("Logged In successfully")
-                    // this.props.history.push('/')
-                }else{
-                    // alert.show(r.data.message[0])
-                    toast.warning(r.data.message[0], {
-                        position: "top-center",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: false,
-                        pauseOnHover: false,
-                        draggable: false,
-                    });
-                }
-                
-            })
-            .catch(e=>{
-                toast.error("Wrong credentials try again!", {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: false,
-                    pauseOnHover: false,
-                    draggable: false,
-                });
-                // alert.error("try again!")
-            })
-        },2000)
-        this.setState(prevState => {return {isActive: !prevState.isActive}})
-    }
-    
-    render(){
-        return(
+      })
+      .then(function(response){
+        that.props.changePage("delete");
+        that.props.updateCurrentUser(email);
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+  }
+  render() {
+    return (
         <div>
-           {/* <Loading isActive={this.state.isActive}></Loading> */}
-           <h1 className='page-title'>Sign In</h1>
-           <Container>
-                <Form>
-                    <FormGroup>
-                        <Label>
-                            Username
-                        </Label>
-                        <Input 
-                            name= {'email'}
-                            value={this.state.email} 
-                            placeholder = {'Enter your email'}
-                            onChange = {e => this.handleChange(e)}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>
-                            Password
-                        </Label>
-                        <Input 
-                            type="password"
-                            name= {'password'}
-                            value={this.state.password} 
-                            placeholder = {'Enter your password'}
-                            onChange = {e => this.handleChange(e)}
-                        />
-                    </FormGroup>
-                    <Button 
-                        onClick= {this.handleSubmit}>    
-                        Sign In
-                    </Button>
-                </Form>
-           </Container>
-           
+          <h2>Signup</h2>
+          <form>
+            <input id="email" placeholder="email"/>
+            <input id="password" placeholder="password"/>
+            <input id="password_confirmation" placeholder="retype password"/>
+            <button onClick={this.handleSignup}>Submit</button>
+          </form>
+          <button onClick={() => this.props.changePage("login")}>Back to Login</button>
         </div>
-        )
-    }
-}
+      );
+    };
+  };
+
+// class Signup extends React.Component {
+//     constructor(props){
+//       super(props);
+//       this.handleSignup = this.handleSignup.bind(this);
+//     }
+//   handleSignup(e) {
+//       e.preventDefault();
+//       let that = this
+//       let email = this.props.currentUser
+//       axios.post('/users', {
+//         user: {
+//           email: document.getElementById("email").value,
+//           password: document.getElementById("password").value,
+//           password_confirmation: document.getElementById("password_confirmation").value
+//         }
+//       })
+//       .then(function(response){
+//         that.props.changePage("delete");
+//         that.props.updateCurrentUser(email);
+//       })
+//       .catch(function(error){
+//         console.log(error)
+//       })
+//   }
+//   render() {
+//     return (
+//         <div>
+//           <h2>Signup</h2>
+//           <form>
+//             <input id="email" placeholder="email"/>
+//             <input id="password" placeholder="password"/>
+//             <input id="password_confirmation" placeholder="retype password"/>
+//             <button onClick={this.handleSignup}>Submit</button>
+//           </form>
+//           <button onClick={() => this.props.changePage("login")}>Back to Login</button>
+//         </div>
+//       );
+//     };
+//   };
+
+// let base_url = 'http://localhost:3000/users/sign_in'
+
+
+// export default class SignIn extends React.Component {
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+//             email:"",
+//             password:"",
+//             isActive: false
+//         }
+//     }
+
+//     handleChange = e => {
+//         e.preventDefault()
+//         this.setState({[e.target.name]: e.target.value})
+//     }
+
+//     handleSubmit = () => {
+//         setTimeout(() => {
+//             this.setState(prevState => {return {isActive: !prevState.isActive}})
+//             axios.post('http://localhost:3000/users/sign_in' , 
+//                 {auth: {email: this.state.email, password: this.state.password}}
+//             )
+//             .then(r=>{
+                
+//                 console.log(r);
+//                 if (r.data.jwt) {
+//                     localStorage.setItem('token', r.data.jwt)
+//                     this.props.loginStatus(true)
+//                     toast.success("Logged In successfully", {
+//                         position: "top-center",
+//                         autoClose: 5000,
+//                         hideProgressBar: false,
+//                         closeOnClick: false,
+//                         pauseOnHover: false,
+//                         draggable: false,
+//                     });
+//                     // alert.success("Logged In successfully")
+//                     // this.props.history.push('/')
+//                 }else{
+//                     // alert.show(r.data.message[0])
+//                     toast.warning(r.data.message[0], {
+//                         position: "top-center",
+//                         autoClose: 5000,
+//                         hideProgressBar: false,
+//                         closeOnClick: false,
+//                         pauseOnHover: false,
+//                         draggable: false,
+//                     });
+//                 }
+                
+//             })
+//             .catch(e=>{
+//                 toast.error("Wrong credentials try again!", {
+//                     position: "top-center",
+//                     autoClose: 5000,
+//                     hideProgressBar: false,
+//                     closeOnClick: false,
+//                     pauseOnHover: false,
+//                     draggable: false,
+//                 });
+//                 // alert.error("try again!")
+//             })
+//         },2000)
+//         this.setState(prevState => {return {isActive: !prevState.isActive}})
+//     }
+    
+//     render(){
+//         return(
+//         <div>
+//            {/* <Loading isActive={this.state.isActive}></Loading> */}
+//            <h1 className='page-title'>Sign In</h1>
+//            <Container>
+//                 <Form>
+//                     <FormGroup>
+//                         <Label>
+//                             Username
+//                         </Label>
+//                         <Input 
+//                             name= {'email'}
+//                             value={this.state.email} 
+//                             placeholder = {'Enter your email'}
+//                             onChange = {e => this.handleChange(e)}
+//                         />
+//                     </FormGroup>
+//                     <FormGroup>
+//                         <Label>
+//                             Password
+//                         </Label>
+//                         <Input 
+//                             type="password"
+//                             name= {'password'}
+//                             value={this.state.password} 
+//                             placeholder = {'Enter your password'}
+//                             onChange = {e => this.handleChange(e)}
+//                         />
+//                     </FormGroup>
+//                     <Button 
+//                         onClick= {this.handleSubmit}>    
+//                         Sign In
+//                     </Button>
+//                 </Form>
+//            </Container>
+           
+//         </div>
+//         )
+//     }
+// }
+
+export default Signin
