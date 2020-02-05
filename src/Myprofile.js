7388a4b30b50307d21046f61ab51504ef846553f
hatimalattas <alattas96@outlook.com>
2020-02-06 00:14:12 +0300

    laaaaaaaaast update

--- src/Myprofile.js
+++ src/Myprofile.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './Myprofile.css';
-import axios from 'axios' ;
 import MyprofileInfo from './MyprofileInfo';
-import jwt_decode from 'jwt-decode'
+import jwt_decode from 'jwt-decode';
 class Myprofile extends Component { 
   constructor(){
     super()
@@ -23,39 +22,24 @@ class Myprofile extends Component {
                      name : user.user.name,
                      email : user.user.email,
                      height : user.user.height,
-                     weight: user.user.height,
+                     weight: user.user.weight,
                      gender: user.user.gender,
                      birthdate: user.user.birthdate
       })
-      // axios.get(`http://localhost:5000/user/${user.user._id}`)
-      //  .then(data =>{
-      //  console.log("Ssssss")
-      //  console.log(data.data)
-      //  this.setState({
-      //       arr:  data.data
-      //  })
-    // }) }
-  // state ={}
-  // componentDidMount(){
-  //   // console.log(jwt_decode(localStorage.token))
-  // }
     }
   render() {
-
     console.log(this.state)
     return (
 
 <div className="row container" >
-
 <MyprofileInfo 
-//  name={item.name}
-//  email={item.email} 
-//  height={item.height} 
-//  weight={item.weight}  
-//  gender={item.gender}  
-//  birthdate={item.birthdate} 
+ name={this.state.name}
+ email={this.state.email} 
+ height={this.state.height} 
+ weight={this.state.weight}  
+ gender={this.state.gender}  
+ birthdate={this.state.birthdate} 
 ></MyprofileInfo>
-
 </div>
 
 );
